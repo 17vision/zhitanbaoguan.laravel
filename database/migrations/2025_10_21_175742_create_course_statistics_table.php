95b32b77c7a0b2d61f92336370f1c9b6bbfb5fab
zhoulin <zhoulin@17vision.com>
2026-01-15 09:53:34 +0800

    adjust

--- database/migrations/2025_10_21_175742_create_course_statistics_table.php
+++ database/migrations/2025_10_21_175742_create_course_statistics_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
             $table->date('date')->comment('观看课程日期');
             $table->unsignedBigInteger('course_id')->comment('课程id');
             $table->unsignedBigInteger('course_chapter_id')->comment('章节id');
-            $table->index(['user_id', 'course_id', 'course_chapter_id', 'date']);
-            $table->unique(['user_id', 'course_id', 'course_chapter_id', 'date']);
+            $table->index(['user_id', 'course_id', 'course_chapter_id', 'date', 'cs_uid_cid_chid_date_index']);
+            $table->unique(['user_id', 'course_id', 'course_chapter_id', 'date'], 'cs_uid_cid_chid_date_unq');
             $table->unsignedInteger('duration')->comment('观看总时长，单位秒');
             $table->unsignedInteger('position')->nullable()->comment('观看位置，单位秒');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->foreign('course_chapter_id')->references('id')->on('course_chapters')->onDelete('cascade');
             $table->timestamps();
         });
     }
