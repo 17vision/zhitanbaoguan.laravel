5d95768619e565bc4800e1fc5df2470c9f2b3cae
zhoulin <zhoulin@17vision.com>
2026-01-16 18:30:37 +0800

    adjust

--- app/Http/Controllers/web/PageController.php
+++ app/Http/Controllers/web/PageController.php
@@ -10,4 +10,13 @@ class PageController extends Controller
     {
         return view('home');
     }
+
+    public function getUser()
+    {
+        return view('activity.user');
+    }
+    public function storeUser()
+    {
+        return 123;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\web\PageController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
+
+Route::get('/activity/user', [PageController::class, 'getUser'])->name('get.activity.user');
+
+Route::post('/activity/user', [PageController::class, 'storeUser'])->name('store.activity.user');
+
