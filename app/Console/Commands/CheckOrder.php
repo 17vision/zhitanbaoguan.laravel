cc19ae88ce50d75447ae6fd5f62f1c600e7ca5dd
zhoulin <zhoulin@17vision.com>
2026-04-07 11:18:19 +0800

    定时任务跑起来

---
+++ app/Console/Commands/CheckOrder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order;
+use Illuminate\Console\Command;
+
+class CheckOrder extends Command
+{
+    protected $signature = 'app:check-order';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        // 如果一分钟内,没支付,就关闭订单
+        Order::query()->where('status', 1)->where('created_at', '<', now()->subMinute())->update(['status' => 0]);
+
+        // 如果用户在体验中,卡了超过 16 分钟,也关闭订单
+        Order::query()->where('status', 2)->where('order_status', 3)->where('created_at', '<', now()->subMinute(16))->update(['order_status' => 0]);
+    }
+}
