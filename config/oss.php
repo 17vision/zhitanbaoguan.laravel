082c20b2fc1141ed77576df240b2b094e6c20894
zhoulin <zhoulin@17vision.com>
2026-04-16 14:03:11 +0800

    adjust

--- config/oss.php
+++ config/oss.php
@@ -2,8 +2,9 @@
 
 return [
     'aliyun' => [
-        'region' => 'cn-shanghai',
-        'bucket' => 'zhi-tan-bao-guan',
+        'region' => env('ALIYUN_OSS_REGION', 'cn-shanghai'),
+        'bucket' => env('ALIYUN_OSS_BUCKET', 'zhi-tan-bao-guan'),
+        'endPoint' => env('ALIYUN_OSS_ENDPOINT'),
         'accessKeyId' => env('ALIYUN_OSS_ACCESS_KEY_ID'),
         'accessKeySecret' => env('ALIYUN_OSS_ACCESS_KEY_SECRET'),
     ]
