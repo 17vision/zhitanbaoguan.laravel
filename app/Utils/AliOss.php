381e6cee531f6e94a38fb83d3634487146142190
zhoulin <zhoulin@17vision.com>
2026-04-16 14:00:04 +0800

    文件上传

--- app/Utils/AliOss.php
+++ app/Utils/AliOss.php
@@ -4,12 +4,13 @@ namespace App\Utils;
 
 use AlibabaCloud\Oss\V2 as Oss;
 use AlibabaCloud\Oss\V2\Models\PutObjectRequest;
+use AlibabaCloud\Oss\V2\Credentials\StaticCredentialsProvider;
 class AliOss
 {
     private $region = 'cn-shanghai';
     private $bucket = 'zhi-tan-bao-guan';
+    private $endPoint = 'https://oss-ztbg.17vision.com';
 
- 
       /**
      * 上传 Laravel $request->file('file') 到 OSS
      * @param mixed $file 前端上传的 $request->file('file')
@@ -27,12 +28,17 @@ class AliOss
             $body = Oss\Utils::streamFor($stream);
 
             // 4. OSS 配置
-            $credentialsProvider = new Oss\Credentials\EnvironmentVariableCredentialsProvider();
+            // $credentialsProvider = new Oss\Credentials\EnvironmentVariableCredentialsProvider();
+            $credentialsProvider = new StaticCredentialsProvider(
+                config('oss.aliyun.accessKeyId'),
+                config('oss.aliyun.accessKeySecret')
+            );
+
             $cfg = Oss\Config::loadDefault();
             $cfg->setCredentialsProvider($credentialsProvider);
             $cfg->setRegion($this->region);
-            $cfg->setEndpoint('https://oss-ztbg.17vision.com');
-            
+            $cfg->setEndpoint($this->endPoint);
+
             $client = new Oss\Client($cfg);
 
             // 5. 上传请求
@@ -66,6 +72,9 @@ class AliOss
      */
     private function getOssFileUrl(string $key): string
     {
+        if ($this->endPoint) {
+            return $this->endPoint . "/" . $key;
+        }
         return "https://{$this->bucket}.oss.{$this->region}.aliyuncs.com/{$key}";
     }
 }
--- config/oss.php
+++ config/oss.php
@@ -3,6 +3,8 @@
 return [
     'aliyun' => [
         'region' => 'cn-shanghai',
-        'bucket' => 'zhi-tan-bao-guan'
+        'bucket' => 'zhi-tan-bao-guan',
+        'accessKeyId' => env('ALIYUN_OSS_ACCESS_KEY_ID'),
+        'accessKeySecret' => env('ALIYUN_OSS_ACCESS_KEY_SECRET'),
     ]
 ];
